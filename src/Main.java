f4335b924f9e926850f9c2830233b6d3bdc5ffc1
Vel <korzhavin.vel@yandex.ru>
2023-07-12 20:50:30 +0700

    Arrays1 task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -20,6 +21,34 @@ public class Main {
         for (int i = 0; i < thirdArray.length; i++) {
             System.out.print(thirdArray[i] + " ");
         }
+        System.out.println();
     }
 
+    public static void task2() {
+        System.out.println("Задача2");
+        int[] firstArray1 = new int[3];
+        firstArray1[0] = 1;
+        firstArray1[1] = 2;
+        firstArray1[2] = 3;
+        System.out.print(firstArray1[0] + ", ");
+        System.out.print(firstArray1[1] + ", ");
+        System.out.print(firstArray1[2]);
+        System.out.println();
+        float[] secondArray1 = {1.57f, 7.654f, 9.986f};
+        for (int i = 0; i < secondArray1.length; i++) {
+            if (i == secondArray1.length - 1) {
+                System.out.println(secondArray1[i]);
+                break;
+            }
+            System.out.print(secondArray1[i] + ", ");
+        }
+        int[] thirdArray1 = {57, 14, 78};
+        for (int i = 0; i < thirdArray1.length; i++) {
+            if (i == thirdArray1.length - 1) {
+                System.out.println(secondArray1[i]);
+                break;
+            }
+            System.out.print(thirdArray1[i] + ", ");
+        }
+    }
 }
